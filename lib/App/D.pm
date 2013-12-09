744f70ed61a8d1beb52d9184a393a34910861b71
Иван Бессарабов <ivan@bessarabov.ru>
2013-12-09 20:08:16 +0000

    Import of BESSARABV/App-D-1.0.0 from CPAN.
    gitpan-cpan-distribution: App-D
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         BESSARABV/App-D-1.0.0.tar.gz
    gitpan-cpan-author:       BESSARABV
    gitpan-cpan-maturity:     released

---
+++ lib/App/D.pm
@@ -0,0 +1,35 @@
+package App::D;
+{
+  $App::D::VERSION = '1.0.0';
+}
+
+# ABSTRACT: show time and month info
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::D - show time and month info
+
+=head1 VERSION
+
+version 1.0.0
+
+=head1 AUTHOR
+
+Ivan Bessarabov <ivan@bessarabov.ru>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Ivan Bessarabov.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
